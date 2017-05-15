0e76e6b4be31855ae516bffca5acd49522e6981d
Johan Walberg <johanwalberg@outlook.com>
2017-05-15 11:23:45 +0200

    blabla

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -88,28 +88,28 @@ function getTheCity (data) {
     return data.results[0].address_components[4].long_name;
 }
 
+// Call the open menu function
 $('#open-menu').click(function() {
     openMenu();
 });
 
+// Call the close menu function
 $('#close-menu').click(function() {
     closeMenu();
 });
 
+// Open menu
 function openMenu() {
-
     $('#open-menu').hide();
     $('#close-menu').show();
-    $('#top-menu').show();
-    
+    $('#top-menu').show(); 
 }
 
+// Close menu
 function closeMenu() {
-
     $('#close-menu').hide();
     $('#open-menu').show();
     $('#top-menu').hide();
-
 }
 // Run function only when page is done loading
 $(document).ready(function(){
