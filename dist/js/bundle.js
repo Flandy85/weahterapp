07cc785bf364429d75bb1885b28629b07713bf2f
Johan Walberg <johanwalberg@outlook.com>
2017-05-15 10:44:07 +0200

    flickr.js skapad

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,3 +1,39 @@
+$("button").on("click", handleButtonClick);
+
+function jsonFlickrFeed(json) {
+  //console.log(json);
+  
+    var flickr = json.items;
+   console.log(flickr[0].media);
+   $("<img />").attr("src", flickr[0].media.m).appendTo("#images"); 
+    
+//  $.each(json.items, function(i, item) {
+//    $("<img />").attr("src", item.media.m).appendTo("#images"); 
+//  });
+};
+
+function handleButtonClick() {
+    
+    var bla = $('#txt_name').val();
+    
+    
+//  $("button").remove();
+  
+  $.ajax({
+    url: 'https://api.flickr.com/services/feeds/photos_public.gne',
+    dataType: 'jsonp',
+    data: { "tags": bla, "format": "json" }
+  });
+}
+
+
+/*
+index.html tagsen...
+   <button id="button">I Love cats!</button>
+   <input type="text" id="txt_name"/>
+
+<div id="images"></div>
+*/
 $(document).ready(function() {
     // Check if browser supports geolocation.
     function getLocation() {
