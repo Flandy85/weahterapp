55d2f83ebf0d44c1c736a856d9dbe6f63238184a
Anders Gustavsson <gustavsson09@hotmail.com>
2017-05-19 15:39:14 +0200

    Optimized click and enter function on menu search function in search.js

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -260,34 +260,49 @@ function checkTime(i) {
 startTime();
 // Run function only when page is done loading
 $(document).ready(function(){
-
     // Click to run search function
-    $('#search-btn').click(function (string) {
-      // selected_size = 800; --------> // KANSKE ÄR KOD SOM SKA ANVÄNDAS FÖR ATT FÅ UT STÖRRE BILDER FRÅN FLICKR, OKLART I DAGSLÄGET!
-        // To get the value of the input field
-        // and turn it to a variable that can be
-        // used for the search.
-        let citySearch = $('#city-name').val();
-        cityConverter(citySearch);
-        // function, Close dropdown menu after click
-        closeMenu();
+    $('#search-btn').click(callback);
+    // Press enter to run search function
+    $("#city-name").keypress(function() {
+        if (event.which === 13) callback();
     });
 
-    // Press enter to run search function
-    $('#city-name').keypress(function (e) {
-        // Does the same as the above function
-        // but listens for the enter key instead.
-        let citySearch = $('#city-name').val();
-        let key = e.which;
-        if(key == 13)  // The enter key code
-        {
-            cityConverter(citySearch);  
-            // function, Close dropdown menu after click
-            closeMenu();
-        }
+    // Click to run search function
+    // $('#search-btn').click(function (string) {
+    //   // selected_size = 800; --------> // KANSKE ÄR KOD SOM SKA ANVÄNDAS FÖR ATT FÅ UT STÖRRE BILDER FRÅN FLICKR, OKLART I DAGSLÄGET!
+    //     // To get the value of the input field
+    //     // and turn it to a variable that can be
+    //     // used for the search.
+    //     let citySearch = $('#city-name').val();
+    //     cityConverter(citySearch);
+    //     // function, Close dropdown menu after click
+    //     closeMenu();
+    // });
 
-    }); 
+    // Press enter to run search function
+    // $('#city-name').keypress(function (e) {
+    //     // Does the same as the above function
+    //     // but listens for the enter key instead.
+    //     let citySearch = $('#city-name').val();
+    //     let key = e.which;
+    //     if(key == 13)  // The enter key code
+    //     {
+    //         cityConverter(citySearch);  
+    //         // function, Close dropdown menu after click
+    //         closeMenu();
+    //     }
+
+    // }); 
+       
 });
+// Variable with function which activates on click or keydown event.
+let callback = function() {
+    // used for the search.
+    let citySearch = $('#city-name').val();
+    cityConverter(citySearch);
+    // function, Close dropdown menu after click or keydown enter
+    closeMenu();
+}
 
 
 function errorMessage(error) {
