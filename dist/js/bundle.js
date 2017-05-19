5c9c875d57ab27f9684d9a061e8bb873fd18689e
Anders Gustavsson <gustavsson09@hotmail.com>
2017-05-19 14:26:31 +0200

    Added comments

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -265,6 +265,7 @@ $(document).ready(function(){
         // used for the search.
         let citySearch = $('#city-name').val();
         cityConverter(citySearch);
+        // function, Close dropdown menu after click
         closeMenu();
     });
 
@@ -277,6 +278,7 @@ $(document).ready(function(){
         if(key == 13)  // The enter key code
         {
             cityConverter(citySearch);  
+            // function, Close dropdown menu after click
             closeMenu();
         }
 
--- src/js/search.js
+++ src/js/search.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         // used for the search.
         let citySearch = $('#city-name').val();
         cityConverter(citySearch);
+        // function, Close dropdown menu after click
         closeMenu();
     });
 
@@ -21,6 +22,7 @@ $(document).ready(function(){
         if(key == 13)  // The enter key code
         {
             cityConverter(citySearch);  
+            // function, Close dropdown menu after click
             closeMenu();
         }
 
