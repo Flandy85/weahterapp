2e8589be898a4289b6b7e3ef40fb54f664718f4e
Johan Walberg <johanwalberg@outlook.com>
2017-05-20 16:02:11 +0200

    bugfix av flicker api

--- src/js/flickr.js
+++ src/js/flickr.js
@@ -21,9 +21,10 @@
 function flickrImg(city) {
     
     // var bla = $('.txt_name').val();
-    $.ajax('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d307b62e26833c7387dc4f1fe10eac6f&text=' + city + '&extras=url_l&per_page=1&page=1&format=json', { dataType: 'jsonp', jsonp: 'jsoncallback' })
+    $.ajax('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dd19ed51d6cdbcda479e49d09494285a&tags=' + city + '&extras=url_l&per_page=1&page=1&format=json', { dataType: 'jsonp', jsonp: 'jsoncallback' })
     .then(function(data, status, xhr) {
-        $('body').css({"background": "url(" +data.photos.photo[0].url_l + ")",
+        //console.log(data);
+        $('body').css({"background": "url(" + data.photos.photo[0].url_l + ")",
     "background-size": "cover",
     "background-repeat": "no-repeat"});
        // console.log('success (promises): ' + data[0]);
