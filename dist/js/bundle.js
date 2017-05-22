7f777b290ddc28a3f8ce9b735ec4aa393a7670a9
Anders Gustavsson <gustavsson09@hotmail.com>
2017-05-22 13:34:21 +0200

    Removed old commented code

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -266,34 +266,7 @@ $(document).ready(function(){
     // Press enter to run search function
     $("#city-name").keypress(function() {
         if (event.which === 13) callback();
-    });
-
-    // Click to run search function
-    // $('#search-btn').click(function (string) {
-    //   // selected_size = 800; --------> // KANSKE ÄR KOD SOM SKA ANVÄNDAS FÖR ATT FÅ UT STÖRRE BILDER FRÅN FLICKR, OKLART I DAGSLÄGET!
-    //     // To get the value of the input field
-    //     // and turn it to a variable that can be
-    //     // used for the search.
-    //     let citySearch = $('#city-name').val();
-    //     cityConverter(citySearch);
-    //     // function, Close dropdown menu after click
-    //     closeMenu();
-    // });
-
-    // Press enter to run search function
-    // $('#city-name').keypress(function (e) {
-    //     // Does the same as the above function
-    //     // but listens for the enter key instead.
-    //     let citySearch = $('#city-name').val();
-    //     let key = e.which;
-    //     if(key == 13)  // The enter key code
-    //     {
-    //         cityConverter(citySearch);  
-    //         // function, Close dropdown menu after click
-    //         closeMenu();
-    //     }
-
-    // }); 
+    }); 
        
 });
 // Variable with function which activates on click or keydown event.
@@ -304,7 +277,6 @@ let callback = function() {
     // function, Close dropdown menu after click or keydown enter
     closeMenu();
 }
-
 
 function errorMessage(error) {
     	
