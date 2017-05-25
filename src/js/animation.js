d41e76a916dd5c17de82d1bef85d651c536c5295
Anders Gustavsson <gustavsson09@hotmail.com>
2017-05-25 22:51:50 +0200

    added console.log in animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	console.log('mja');
+	console.log('testing animation.js');
 	setInterval(function () {
         $('#raindrops').fadeIn(50).delay(50).fadeOut().delay(50).fadeIn(100);
     }, 5000);
