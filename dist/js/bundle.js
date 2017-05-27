23e87ab3658bdb895cdf4fa62913d8fd6cea0283
Anders Gustavsson <gustavsson09@hotmail.com>
2017-05-27 12:45:09 +0200

    Removed comments in flickr.js

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -54,25 +54,6 @@ function theMonth(thisMonth) {
 }
 
 dayDateMonth();
-// Function jsonFlickrFeed is activated when user use searchfunction in weather.js
-//function jsonFlickrFeed(json) {
-//  //console.log(json);
-//  
-//    const flickr = json.items;
-//   //console.log(flickr[0].media);
-//   // $("#images").html('<img src="' + flickr[0].media.m + '">');
-//   $('body').css({"background": "url(" +data.photos.photo[0].url_l + ")",
-//    "background-size": "cover",
-//    "background-repeat": "no-repeat"
-//
-//
-//    });
-//};
-// function that activates ajax call to flickr api when searchbutton i clicked.
-
-
-// KANSKE ÄR KOD SOM SKA ANVÄNDAS FÖR ATT FÅ UT STÖRRE BILDER FRÅN FLICKR, OKLART I DAGSLÄGET!
-// var apiurl,myresult,apiurl_size,selected_size;  
 
 function flickrImg(city) {
     
@@ -84,31 +65,9 @@ function flickrImg(city) {
     "background-size": "cover",
     "background-repeat": "no-repeat"});
        // console.log('success (promises): ' + data[0]);
-}, function(xhr, status, error) {
-    console.log('failed (promises): ' + error);
-});
-
-
-
-
-// KANSKE ÄR KOD SOM SKA ANVÄNDAS FÖR ATT FÅ UT STÖRRE BILDER FRÅN FLICKR, OKLART I DAGSLÄGET!
-  // $('#search-btn').click(function(){  
-    // $.getJSON(apiurl,function(json){  
-    //   $.each(json.photos.photo,function(i,myresult){  
-    //     apiurl_size = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=cbbd48e2830e6787ff24a776d11985ba&photo_id="+myresult.id+"&format=json&nojsoncallback=1";  
-    //     $.getJSON(apiurl_size,function(size){  
-    //       $.each(size.sizes.size,function(i,myresult_size){  
-    //         if(myresult_size.width==selected_size){  
-    //           $("body").append('<p><a href="'+myresult_size.url+'" target="_blank"><img src="'+myresult_size.source+'"/></a></p>');
-
-    //           console.log(myresult_size.url + myresult_size.source); 
-    //         }  
-    //       })  
-    //     })  
-    //   });  
-    // });  
-  // }); 
-
+    }, function(xhr, status, error) {
+        console.log('failed (promises): ' + error);
+    });
 
 }
 
